b5814be06b9ed6230675bee3201c61d1754599ba
SysWhiteDev <syswhite.commercial@gmail.com>
2023-10-15 20:31:11 +0000

    Fixed unrecognized archive error

--- update.sh
+++ update.sh
@@ -3,3 +3,6 @@
 sudo rm -rf ./x86_64/hidecore.*
 cd ./x86_64
 repo-add hidecore.db.tar.gz *.pkg.tar.zst
+sudo rm -rf hidecore.db hidecore.files
+mv hidecore.db.tar.gz hidecore.db
+mv hidecore.files.tar.gz hidecore.files
